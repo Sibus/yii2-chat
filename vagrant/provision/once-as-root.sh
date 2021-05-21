83a9e468901ce7bca333e7f39201693724033b68
sibus <persibus@gmail.com>
2021-05-21 15:00:09 +0300

    update php up to 8.0

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -28,8 +28,9 @@ info "Update OS software"
 apt-get update
 apt-get upgrade -y
 
+add-apt-repository ppa:ondrej/php
 info "Install additional software"
-apt-get install -y php7.0-curl php7.0-cli php7.0-intl php7.0-mysqlnd php7.0-gd php7.0-fpm php7.0-mbstring php7.0-xml unzip nginx mysql-server-5.7 php.xdebug
+apt-get install -y php8.0-curl php8.0-cli php8.0-intl php8.0-mysqlnd php8.0-gd php8.0-fpm php8.0-mbstring php8.0-xml unzip nginx mysql-server-5.7 php.xdebug
 
 info "Configure MySQL"
 sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -40,10 +41,10 @@ mysql -uroot <<< "FLUSH PRIVILEGES"
 echo "Done!"
 
 info "Configure PHP-FPM"
-sed -i 's/user = www-data/user = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/group = www-data/group = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-cat << EOF > /etc/php/7.0/mods-available/xdebug.ini
+sed -i 's/user = www-data/user = vagrant/g' /etc/php/8.0/fpm/pool.d/www.conf
+sed -i 's/group = www-data/group = vagrant/g' /etc/php/8.0/fpm/pool.d/www.conf
+sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/8.0/fpm/pool.d/www.conf
+cat << EOF > /etc/php/8.0/mods-available/xdebug.ini
 zend_extension=xdebug.so
 xdebug.remote_enable=1
 xdebug.remote_connect_back=1
